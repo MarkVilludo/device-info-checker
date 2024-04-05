7e503dc42777e95e858ef94e7a3fb55c50cee352
Mark Villudo <markanthony.villudo@gmail.com>
2024-04-05 10:57:52 +0800

    .

--- app/Http/Controllers/ClientDataController.php
+++ app/Http/Controllers/ClientDataController.php
@@ -9,7 +9,6 @@ use Carbon\Carbon;
 
 class ClientDataController extends Controller
 {
-    //
     public function __construct(DeviceInformation $deviceInformation) {
         $this->deviceInformation = $deviceInformation;
     }
@@ -40,56 +39,6 @@ class ClientDataController extends Controller
         return response()->json(['success' => true, 'message' => 'Successfully saved client data'], 200);
     }
     
-    // private function calculateSimilarityScore($requestData, $client)
-    // {
-    //     $matchingFields = [];
-    //     $totalFields = count($requestData);
-        
-    //     // Iterate through the fields in the requestData
-    //     foreach ($requestData as $key => $value) {
-    //         // Exclude the "ip" field from comparison
-    //         if ($key !== 'ip') {
-    //             // Check if the field exists in the client object
-    //             if (isset($client->$key)) {
-    //                 // Compare the field value with the corresponding field in the database
-    //                 if (is_bool($value)) {
-    //                     // If the value is boolean, compare it directly
-    //                     if ($client->$key === $value) {
-    //                         $matchingFields[] = 1;
-    //                     }
-    //                 } else {
-    //                     // If the value is not boolean, compare as string
-    //                     if ($client->$key == $value) {
-    //                         $matchingFields[] = 1;
-    //                     }
-    //                 }
-    //             } else {
-    //                 // If any field doesn't exist in the client object, return 0 similarity
-    //                 $matchingFields[] = 0;
-    //             }
-    //         }
-    //     }
-
-    //     $matchCountField = array_sum($matchingFields);
-        
-    //     // Calculate similarity score as a percentage
-    //     $similarityScore = ($matchCountField / ($totalFields - 1)) * 100; // Exclude the "ip" field from totalFields
-        
-    //     // Check if the similarity score is 100, indicating that all fields match
-    //     if ($similarityScore === 100) {
-    //         // Check if there are additional fields in the client object that are not in requestData
-    //         foreach ($client as $key => $value) {
-    //             if ($key !== 'ip' && !isset($requestData[$key])) {
-    //                 // If any additional field exists in the client object, return 0 similarity
-    //                 return 0;
-    //             }
-    //         }
-    //     }
-        
-    //     return $similarityScore;
-    // }
-
-
     private function calculateSimilarityScore($requestData, $client)
     {
         $matchingFields = [];
@@ -102,30 +51,24 @@ class ClientDataController extends Controller
                 // Check if the field exists in the client object
                 if (isset($client->$key)) {
                     // Increment matchingFields if the field value matches
-                    // if ($client->$key === $value) {
-                    //     $matchingFields[] = 1;
-                    // }
                     if (is_bool($value)) {
                         // If the value is boolean, compare it directly
                         if ($client->$key === $value) {
                             $matchingFields[] = 1;
                         }
                     } else {
-                        // If the value is not boolean, compare as string
+                        // If the value is not boolean, compare the value only not including the data types
                         if ($client->$key == $value) {
                             $matchingFields[] = 1;
                         }
                     }
-
                 } else {
                     // If any field doesn't exist in the client object, return 0 similarity
                     $matchingFields[] = 0;
                 }
             }
         }
-
         $matchCountField = array_sum($matchingFields);
-        
         // Calculate similarity score as a percentage
         return ($matchCountField / ($totalFields - 1)) * 100; // Exclude the "ip" field from totalFields
     }
