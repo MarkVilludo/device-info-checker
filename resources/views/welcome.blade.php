932b2bdd82b39173c428996854b97f4c3bf94dc5
Mark Villudo <markanthony.villudo@gmail.com>
2024-04-10 13:26:25 +0800

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,15 +10,6 @@
     <h1>Device Information</h1>
     <div id="result"></div>
     <script>
-
-        async function getNetworkType() {
-            if (navigator.connection && navigator.connection.getEffectiveType) {
-                return navigator.connection.getEffectiveType();
-            } else {
-                console.warn("Network Information API not supported");
-                return "Unknown";
-            }
-        }
         async function fetchIpAddress() {
             try {
                 const response = await fetch('https://api.ipify.org?format=json');
@@ -36,20 +26,36 @@
             // Return the resolution parts in reversed order
             return `${height}x${width}`;
         }
+
+        async function guessNetworkType() {
+            if ('connection' in navigator) {
+                const connection = navigator.connection;
+                if (connection.effectiveType) {
+                    return connection.effectiveType;
+                }
+            }
+            const userAgent = navigator.userAgent;
+            if (/LTE/i.test(userAgent)) {
+                return "4G/LTE";
+            } else if (/3G/i.test(userAgent)) {
+                return "3G";
+            } else {
+                return "Unknown";
+            }
+        }
        
         async function getInfo() {
             const ip = await fetchIpAddress();
-            const network = getNetworkType();
-            console.log(network)
             // in a browser, when using a script tag:
             const ClientJS = window.ClientJS;
             // Create a new ClientJS object
             const client = new ClientJS();
             const availResolution = await reverseInfo(client.getAvailableResolution());
             const landscape = await reverseInfo(client.getCurrentResolution());
+            const batteryLevel = (await navigator.getBattery()).level * 100;
+            const network = await guessNetworkType();
             const info = [
-                // client.getBrowserData(),
-                client.getFingerprint(),
+              
                 client.getCustomFingerprint(
                     client.getTimeZone(),
                     client.getLanguage(),
@@ -78,6 +84,8 @@
                     ip,
                     client.getAvailableResolution(),
                     client.getCurrentResolution(),
+                    batteryLevel,
+                    network
                 ),
                 client.getCustomFingerprint(
                     client.getTimeZone(),
@@ -107,97 +115,57 @@
                     ip,
                     availResolution,
                     landscape,
+                    batteryLevel,
+                    network
                 ),
-                client.getUserAgent(),
-                client.getUserAgentLowerCase(),
-                client.getBrowser(),
-                client.getBrowserVersion(),
-                client.getBrowserMajorVersion(),
-                client.isIE(),
-                client.isChrome(),
-                client.isFirefox(),
-                client.isSafari(),
-                client.isOpera(),
-                client.getEngine(),
-                client.getEngineVersion(),
-                client.getOS(),
-                client.getOSVersion(),
-                client.isWindows(),
-                client.isMac(),
-                client.isLinux(),
-                client.isUbuntu(),
-                client.isSolaris(),
-                client.getDevice(),
-                client.getDeviceType(),
-                client.getDeviceVendor(),
-                client.getCPU(),
-                client.isMobile(),
-                client.isMobileMajor(),
-                client.isMobileAndroid(),
-                client.isMobileOpera(),
-                client.isMobileWindows(),
-                client.isMobileBlackBerry(),
-                client.isMobileIOS(),
-                client.isIphone(),
-                client.isIpad(),
-                client.isIpod(),
-                client.getScreenPrint(),
-                client.getColorDepth(),
-                client.getCurrentResolution(),
-                client.getAvailableResolution(),
-                client.getDeviceXDPI(),
-                client.getDeviceYDPI(),
-                client.getPlugins(),
-                client.isJava(),
-                client.getJavaVersion(), // functional only in java and full builds, throws an error otherwise
-                client.isFlash(),
-                client.getFlashVersion(), // functional only in flash and full builds, throws an error otherwise
-                client.isSilverlight(),
-                client.getSilverlightVersion(),
-                client.getMimeTypes(),
-                client.isMimeTypes(),
-                client.isFont(),
-                client.getFonts(),
-                client.isLocalStorage(),
-                client.isSessionStorage(),
-                client.isCookie(),
-                client.getTimeZone(),
-                client.getLanguage(),
-                client.getSystemLanguage(),
-                client.isCanvas(),
-                client.getCanvasPrint(),
-                ip,
-                availResolution,
-                landscape,
+                    client.getBrowserMajorVersion(),
+                    client.getTimeZone(),
+                    client.getLanguage(),
+                    client.getScreenPrint(),
+                    client.getFonts(),
+                    client.getOS(),
+                    client.getEngine(),
+                    client.isCookie(),
+                    client.isSessionStorage(),
+                    client.isCanvas(),
+                    client.isSilverlight(),
+                    client.isMobile(),
+                    client.isMobileMajor(),
+                    client.isMobileAndroid(),
+                    client.isMobileOpera(),
+                    client.isMobileWindows(),
+                    client.isMobileBlackBerry(),
+                    client.isMobileIOS(),
+                    client.isIphone(),
+                    client.isIpad(),
+                    client.isIpod(),
+                    client.getDeviceType(),
+                    client.isWindows(),
+                    client.isMac(),
+                    client.isLinux(),
+                    client.isUbuntu(),
+                    ip,
+                    client.getAvailableResolution(),
+                    client.getCurrentResolution(),
+                    availResolution,
+                    landscape,
+                    batteryLevel,
+                    network
             ];
             const distinct = [
-                // 'getBrowserData',
-                'getFingerprint',
-                'getCustomFingerprintCurrentResolution',
-                'getCustomFingerprintAvailableResolution',
-                'getUserAgent',
-                'getUserAgentLowerCase',
-                'getBrowser',
-                'getBrowserVersion',
+                'getCustomFingerprintPortrait',
+                'getCustomFingerprintLandscape',
                 'getBrowserMajorVersion',
-                'isIE',
-                'isChrome',
-                'isFirefox',
-                'isSafari',
-                'isOpera',
-                'getEngine',
-                'getEngineVersion',
+                'getTimeZone',
+                'getLanguage',
+                'getScreenPrint',
+                'getFonts',
                 'getOS',
-                'getOSVersion',
-                'isWindows',
-                'isMac',
-                'isLinux',
-                'isUbuntu',
-                'isSolaris',
-                'getDevice',
-                'getDeviceType',
-                'getDeviceVendor',
-                'getCPU',
+                'getEngine',
+                'isCookie',
+                'isSessionStorage',
+                'isCanvas',
+                'isSilverlight',
                 'isMobile',
                 'isMobileMajor',
                 'isMobileAndroid',
@@ -208,34 +176,18 @@
                 'isIphone',
                 'isIpad',
                 'isIpod',
-                'getScreenPrint',
-                'getColorDepth',
-                'getCurrentResolution',
+                'getDeviceType',
+                'isWindows',
+                'isMac',
+                'isLinux',
+                'isUbuntu',
+                'ip',
                 'getAvailableResolution',
-                'getDeviceXDPI',
-                'getDeviceYDPI',
-                'getPlugins',
-                'isJava',
-                'getJavaVersion',
-                'isFlash',
-                'getFlashVersion',
-                'isSilverlight',
-                'getSilverlightVersion',
-                'getMimeTypes',
-                'isMimeTypes',
-                'isFont',
-                'getFonts',
-                'isLocalStorage',
-                'isSessionStorage',
-                'isCookie',
-                'getTimeZone',
-                'getLanguage',
-                'getSystemLanguage',
-                'isCanvas',
-                'getCanvasPrint',
-                'ipAddress',
-                'getCurrentResolutionModify',
-                'getAvailableResolutionModify',
+                'getCurrentResolution',
+                'availResolution',
+                'landscape',
+                'batteryLevel',
+                'network'
             ];
             const fingerprintWithName = Object.fromEntries(distinct.map((key, index) => [key, info[index]]));
             const resultDiv = document.getElementById('result');
